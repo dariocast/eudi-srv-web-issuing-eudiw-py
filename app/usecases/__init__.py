248790661a40d34bf303daff85be28a82b48365b
Dario Castellano <dario.castellano@etuitus.it>
2025-03-05 17:40:33 +0100

    feat: top use case /identification

--- app/usecases/__init__.py
+++ app/usecases/__init__.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 
 # Import each use case blueprint
 from .health_usecase import health_usecase
+from .top_usecase import top_usecase
 
 usecases_bp = Blueprint("usecases", __name__, url_prefix="/usecases")
 
 # Register each use case blueprint
 usecases_bp.register_blueprint(health_usecase)
+usecases_bp.register_blueprint(top_usecase)
