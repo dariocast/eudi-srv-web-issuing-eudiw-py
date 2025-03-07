b86073f61ef12a031e092efb8d2d40e4d26a4435
Dario Castellano <dario.castellano@etuitus.it>
2025-03-07 09:12:00 +0100

    feat: ewc usecase with redirect to identification

--- app/usecases/__init__.py
+++ app/usecases/__init__.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 
 # Import each use case blueprint
 from .health_usecase import health_usecase
-from .top_usecase import top_usecase
+from .ewc import ewc_bp
 
 usecases_bp = Blueprint("usecases", __name__, url_prefix="/usecases")
 
 # Register each use case blueprint
 usecases_bp.register_blueprint(health_usecase)
-usecases_bp.register_blueprint(top_usecase)
+usecases_bp.register_blueprint(ewc_bp)
